c4f6318e1af14d5b7b744551daf2b9e7549d51cd
AleksandrDmr <39705669+AleksandrDmr@users.noreply.github.com>
2018-07-02 03:11:24 +0600

    Add files via upload

---
+++ videoglaz/js/script.js
@@ -0,0 +1,143 @@
+$(function(){
+
+
+
+	$('.slider').slick({
+		 dots: false,
+		 infinite: true,
+		 speed: 500,
+		 fade: false,
+		 autoplay: true,
+		 autoplaySpeed: 3000,
+		 cssEase: 'linear',
+		 nextArrow: '<button class="slider-btn slider-btn_prev" type="button"><img src="img/next.png" alt="Назад"></button>',
+		 prevArrow: '<button class="slider-btn slider-btn_next" type="button"><img src="img/prev.png" alt="Вперед"></button>'
+	});
+
+	$('.slider-menu').slick({
+		 slidesToShow: 3,
+		 infinite: false,
+		 arrows: false,
+		 dots: false,
+		 fade: false,
+		 cssEase: 'linear',
+		 responsive: [
+		     {
+		       breakpoint: 780,
+		       settings: {
+		        slidesToShow: 2,
+		       }
+		     }
+		]
+	});
+
+	$('.recommended-slider').slick({
+		 slidesToShow: 5,
+		 infinite: false,
+		 dots: false,
+		 fade: false,
+		 arrows: false,
+		 cssEase: 'linear',
+		 responsive: [
+		     {
+		       breakpoint: 1300,
+		       settings: {
+		         slidesToShow: 3,
+		         arrows: true,
+		         nextArrow: '<button class="slider-btn slider-btn_prev slider-btn_product" type="button"><img src="img/next-product.png" alt="Назад"></button>',
+		 		 prevArrow: '<button class="slider-btn slider-btn_next slider-btn_product" type="button"><img src="img/prev-product.png" alt="Вперед"></button>'
+		       }
+		     },
+		     {
+		       breakpoint: 960,
+		       settings: {
+		         slidesToShow: 2,
+		         arrows: true,
+		         nextArrow: '<button class="slider-btn slider-btn_prev slider-btn_product" type="button"><img src="img/next-product.png" alt="Назад"></button>',
+		 		 prevArrow: '<button class="slider-btn slider-btn_next slider-btn_product" type="button"><img src="img/prev-product.png" alt="Вперед"></button>'
+		       }   
+		    },
+		     {
+		       breakpoint: 780,
+		       settings: {
+		         slidesToShow: 1, 
+		         arrows: true,
+		         nextArrow: '<button class="slider-btn slider-btn_prev slider-btn_product" type="button"><img src="img/next-product.png" alt="Назад"></button>',
+		 		 prevArrow: '<button class="slider-btn slider-btn_next slider-btn_product" type="button"><img src="img/prev-product.png" alt="Вперед"></button>'
+		       }   
+		    }
+
+		]
+	});
+
+	$('.products-slider').slick({
+		infinite:true,
+		autoplay: true,
+		 slidesToShow: 5,
+		 dots: false,
+		 fade: false,
+		 arrows: false,
+		 cssEase: 'linear',
+		 responsive: [
+		     {
+		       breakpoint: 1300,
+		       settings: {
+		       	arrows: true,
+		         slidesToShow: 3,
+		         nextArrow: '<button class="slider-btn slider-btn_prev slider-btn_product" type="button"><img src="img/next-product.png" alt="Назад"></button>',
+		 		 prevArrow: '<button class="slider-btn slider-btn_next slider-btn_product" type="button"><img src="img/prev-product.png" alt="Вперед"></button>'
+		       }
+		     },
+		     {
+		       breakpoint: 960,
+		       settings: {
+		         slidesToShow: 2,
+		         arrows: true,
+		         nextArrow: '<button class="slider-btn slider-btn_prev slider-btn_product" type="button"><img src="img/next-product.png" alt="Назад"></button>',
+		 		 prevArrow: '<button class="slider-btn slider-btn_next slider-btn_product" type="button"><img src="img/prev-product.png" alt="Вперед"></button>'
+		       }   
+		    },
+		     {
+		       breakpoint: 780,
+		       settings: {
+		       	 arrows: true,
+		         slidesToShow: 1, 
+		         nextArrow: '<button class="slider-btn slider-btn_prev slider-btn_product" type="button"><img src="img/next-product.png" alt="Назад"></button>',
+		 		 prevArrow: '<button class="slider-btn slider-btn_next slider-btn_product" type="button"><img src="img/prev-product.png" alt="Вперед"></button>'
+		       }   
+		    }
+
+		]
+	});
+
+
+	
+	function MenuDisplay(obj, obj1, classVisible) {
+		this.display = function() {
+			$(obj).on('click', function(){
+				$(obj1).slideToggle();
+			});
+
+			$(window).resize(function() {
+				if($(window).width() < 980) {
+			    	$(obj1).slideUp();
+			    	console.log($(window).width());
+				}
+			});
+		}
+	}	
+
+	
+	var asideMenu = new MenuDisplay('.aside-btn', '.catalog', "visible");
+	asideMenu.display();
+	var asideMenu = new MenuDisplay('.menu-btn-mobile', '.menu-mobile', "visible");
+	asideMenu.display();
+
+
+
+
+});
+
+
+
+
