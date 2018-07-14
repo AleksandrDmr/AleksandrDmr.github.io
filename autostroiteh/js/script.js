a55e1543fcffb99ca9e4273117030c2ab1457c6f
AleksandrDmr <39705669+AleksandrDmr@users.noreply.github.com>
2018-07-14 18:27:05 +0600

    Add files via upload

---
+++ autostroiteh/js/script.js
@@ -0,0 +1,26 @@
+window.onload = (function(){
+
+
+	$('.slider').slick({
+		 dots: false,
+		 infinite: true,
+		 speed: 500,
+		 fade: false,
+		 autoplay: true,
+		 autoplaySpeed: 3000,
+		 cssEase: 'linear',
+		 arrows: false
+		
+	});
+
+});
+
+
+
+	
+	
+
+
+
+	
+
