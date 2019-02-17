2344f211508d939e145436a9b5c3908fb3debb1b
AleksandrDmr <39705669+AleksandrDmr@users.noreply.github.com>
2019-02-18 02:41:37 +0500

    Add files via upload

--- cyberzone/scripts/script.js
+++ cyberzone/scripts/script.js
@@ -1,5 +1,5 @@
 $(function() {
-	if (window.locale.href = 'https://github.com/AleksandrDmr/AleksandrDmr.github.io/tree/master/cyberzone/') {
+	if (window.location.href = 'https://github.com/AleksandrDmr/AleksandrDmr.github.io/tree/master/cyberzone/') {
 
 
 		$('.wrapper-dots li').on('click', function(event) {
